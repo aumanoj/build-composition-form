5a5500444d3898d8429083b4544da46db4be57a3
Manoj Yadav <aumanojyadav@gmail.com>
2023-09-19 00:33:24 +0530

    Updated layout

--- src/components/FormControls.vue
+++ src/components/FormControls.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 export default {
   setup() {
+    // Form data Object
     const formData = ref({
       name: '',
       mailNotification: false,
@@ -26,10 +27,8 @@ export default {
         errors.value.message = 'Message is required'
         return
       }
-
       // Clear any previous errors
       errors.value = {}
-
       // Form is valid; proceed with submission
       submitted.value = true
       alert('From submitted successfully!')
@@ -45,15 +44,16 @@ export default {
 </script>
 
 <template>
-  <div>
+  <div class="center-box">
     <h1>Composition Form</h1>
-    <hr />
     <form @submit.prevent="submitForm">
+      <!-- Name Field -->
       <div class="form-group">
         <label for="name">Name:</label>
         <input type="text" id="name" v-model="formData.name" />
         <span class="error" v-if="errors.name && !formData.name">{{ errors.name }}</span>
       </div>
+      <!-- Country Field -->
       <div class="form-group">
         <label for="country">Country:</label>
         <select id="country" v-model="formData.country">
@@ -63,16 +63,18 @@ export default {
         </select>
         <span class="error" v-if="errors.country && !formData.country">{{ errors.country }}</span>
       </div>
+      <!-- Message Field -->
       <div class="form-group">
         <label for="messaage">Message:</label>
         <textarea rows="4" cols="50" id="message" v-model="formData.message"></textarea>
         <div class="error" v-if="errors.message">{{ errors.message }}</div>
       </div>
-      <div class="form-group">
+      <!-- Get notification Field -->
+      <div class="form-check">
         <label for="mailNotification">Get notification at mail:</label>
         <input type="checkbox" id="mailNotification" v-model="formData.mailNotification" />
       </div>
-      
+      <!-- Submit button -->
       <button class="button" type="submit">Submit</button>
     </form>
   </div>
@@ -80,45 +82,4 @@ export default {
 
 
 <style scoped>
-.form {
-  width: 300px;
-  margin: 10 auto;
-  padding: 30px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-}
-
-.form-group {
-  margin-bottom: 15px;
-}
-.button {
-  background-color: #008cba;
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  cursor: pointer;
-}
-
-label {
-  display: block;
-  font-weight: bold;
-}
-
-input[type='text'],
-select {
-  width: 100%;
-  padding: 5px;
-  border: 1px solid #ccc;
-  border-radius: 3px;
-}
-
-.error {
-  color: red;
-}
 </style>
